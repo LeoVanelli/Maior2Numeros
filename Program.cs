29d304d236241a4223f1b9eb1dfaf2aaa7a7c983
Leandro Gonçalves Vanelli <55560279+LeoVanelli@users.noreply.github.com>
2021-11-03 19:57:06 -0300

    Adiciona atividade Maior2Numeros

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Maior2Numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o primeiro número: ");
+            Single.TryParse(Console
+            .ReadLine()
+            .ToString(), out Single _primeiroNumero );
+
+            Console.Write("Digite o segundo número: ");
+            Single.TryParse(Console
+            .ReadLine()
+            .ToString(), out Single _segundoNumero );
+
+            Single _maiorNumero = (_primeiroNumero >= _segundoNumero) ? _primeiroNumero : _segundoNumero;
+            Console.WriteLine($"Maior número: {_maiorNumero}");
+        }
+    }
+}
